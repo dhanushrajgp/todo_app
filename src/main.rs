8925b8c994c71bdddda911b4965d120b383df49f
dhanushrajgp <gdhanush68@gmail.com>
2024-07-14 21:29:03 +0530

    created TaskStatus enum,Done and Pending stuct along with Base struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod to_do;
+use to_do::structs::done::Done;
+use to_do::structs::pending::Pending;
 fn main() {
     println!("Hello, world!");
+
+    let done = Done::new("Shopping");
+    println!("{}", done.super_struct.title);
+    println!("{}", done.super_struct.status._stringify());
+    let pending = Pending::new("study");
+    println!("{}", pending.super_struct.title);
+    println!("{}", pending.super_struct.status._stringify());
 }
---
+++ src/to_do/enums.rs
@@ -0,0 +1,12 @@
+pub enum TaskStatus {
+    DONE,
+    PENDING,
+}
+impl TaskStatus {
+    pub fn _stringify(&self) -> String {
+        match &self {
+            &Self::DONE => "DONE".to_string(),
+            &Self::PENDING => "PENDING".to_string(),
+        }
+    }
+}
