a907949307f492f68b0b574a68b4eb9925984577
dhanushrajgp <gdhanush68@gmail.com>
2024-07-14 22:03:31 +0530

    updated main as per factory pattern

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 mod to_do;
-use to_do::structs::done::Done;
-use to_do::structs::pending::Pending;
+use to_do::enums::TaskStatus;
+use to_do::to_do_factory;
+use to_do::ItemTypes;
 fn main() {
-    println!("Hello, world!");
+    let to_do_item = to_do_factory("shopping", TaskStatus::PENDING);
 
-    let done = Done::new("Shopping");
-    println!("{}", done.super_struct.title);
-    println!("{}", done.super_struct.status._stringify());
-    let pending = Pending::new("study");
-    println!("{}", pending.super_struct.title);
-    println!("{}", pending.super_struct.status._stringify());
+    match to_do_item {
+        ItemTypes::Done(item) => {
+            println!("{}", item.super_struct.title);
+            println!("{}", item.super_struct.status._stringify());
+        }
+        ItemTypes::Pending(item) => {
+            println!("{}", item.super_struct.title);
+            println!("{}", item.super_struct.status._stringify());
+        }
+    }
 }
